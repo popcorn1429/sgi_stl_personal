4a4b6b1ee8a2a93a420ea34d6dbad82dfca6ed11
popcorn1429 <30331532+popcorn1429@users.noreply.github.com>
2017-07-21 16:34:48 +0800

    Create stl_relops.h

---
+++ stl_relops.h
@@ -0,0 +1,35 @@
+/*
+it's an internal header file, included by other stl headers;
+you should not attempt to use it directly
+*/
+
+#ifndef __SGI_STL_INTERNAL_RELOPS_H_
+#define __SGI_STL_INTERNAL_RELOPS_H_
+
+#include "stl_config.h"
+
+__STL_BEGIN_RELOPS_NAMESPACE
+
+template <typename T>
+inline bool operator!=(const T& x, const T& y) {
+    return !(x == y);
+}
+
+template <typename T>
+inline bool operator>(const T& x, const T& y) {
+    return y < x;
+}
+
+template <typename T>
+inline bool operator<=(const T& x, const T& y) {
+    return !(y < x);
+}
+
+template <typename T>
+inline bool operator>=(const T& x, const T& y) {
+    return !(x < y);
+}
+
+__STL_END_RELOPS_NAMESPACE
+
+#endif
