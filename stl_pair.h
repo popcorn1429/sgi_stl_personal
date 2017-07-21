41a6b0020698010e620c9d85fe999d3b4936d301
popcorn1429 <30331532+popcorn1429@users.noreply.github.com>
2017-07-21 10:43:53 +0800

    Create stl_pair.h

---
+++ stl_pair.h
@@ -0,0 +1,60 @@
+/*
+it's an internal header file, included by other stl headers;
+you should not attempt to use it directly
+*/
+#ifndef __SGI_STL_INTERNAL_PAIR_H_
+#define __SGI_STL_INTERNAL_PAIR_H_
+#include "stl_config.h"
+
+__STL_BEGIN_NAMESPACE
+
+template <typename T1, typename T2>
+struct pair {
+    using first_type = T1;
+    using second_type = T2;
+
+    T1 first;
+    T2 second;
+
+    pair() : first(T1()), second(T2()) {}
+    pair(const T1& t1, const T2& t2) : first(t1), second(t2) {}
+
+#ifdef __STL_MEMBER_TEMPLATES
+    template <typename U1, typename U2>
+    pair(const pair<U1,U2>& p) : first(p.first), second(p.second) {}
+#endif
+};
+
+template <typename T1, typename T2>
+inline bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y) {
+    return x.first == y.first && x.second == y.second;
+}
+
+template <typename T1, typename T2>
+inline bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y) {
+    return x.first < y.first ||
+        (!(y.first < x.first) && x.second < y.second);
+}
+
+#ifdef __STL_FUNCTION_TMPL_PARTIAL_ORDER
+template <typename T1, typename T2>
+inline bool operator!=(const pair<T1, T2>& x, const pair<T1, T2>& y) {
+    return !(x == y);
+}
+
+template <typename T1, typename T2>
+inline bool operator>(const pair<T1, T2>& x, const pair<T1, T2>& y) {
+    return y < x;
+}
+
+
+
+#endif
+
+
+
+
+__STL_END_NAMESPACE
+
+
+#endif
